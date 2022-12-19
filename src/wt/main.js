5a2c727fa08361ef10c742319a1bd7a4853bad9c
KrisKip123 <kriskip12393@gmail.com>
2022-12-19 12:46:51 +0300

    feat: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import {
+  Worker,
+  isMainThread,
+  workerData,
+  parentPort,
+} from "node:worker_threads";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { cpus } from "node:os";
+
+const nameFile = "worker.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+  const workersArray = await Promise.allSettled(
+    cpus().map((_, index) => {
+      return new Promise((res, rej) => {
+        const worker = new Worker(resolve(__dirname, nameFile), {
+          workerData: { n: 10 + index },
+        });
+        worker.on("error", (mes) => {
+          rej(mes);
+        });
+        worker.on("message", (mes) => {
+          res(mes);
+        });
+      });
+    })
+  );
+  const result = workersArray.map(({ status, value }) => {
+    return {
+      status: status === "fulfilled" ? "resolved" : "error",
+      data: status === "fulfilled" ? value : null,
+    };
+  });
+  console.log(result);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,31 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import {
+  Worker,
+  isMainThread,
+  workerData,
+  parentPort,
+} from "node:worker_threads";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const nameFile = "worker.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (isMainThread) {
+    const worker = new Worker(resolve(__dirname, nameFile), {
+      workerData: { n: 10 },
+    });
+    worker.on("message", (result) => {
+      console.log(result);
+    });
+  } else {
+    const { n } = workerData;
+    parentPort.postMessage(nthFibonacci(n));
+  }
 };
 
 sendResult();
